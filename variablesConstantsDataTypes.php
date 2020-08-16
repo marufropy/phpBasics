7752728256741dfe9b385952029a5597ea0da457
marufropy <marufropy@gmail.com>
2020-08-16 21:49:23 +0600

    variables, constants and data types

---
+++ variablesConstantsDataTypes.php
@@ -0,0 +1,27 @@
+<?php
+	//single line comment
+	#another single line comment
+	/*
+	multi
+	line
+	comment
+	*/
+
+	$str1 = 'Hello';
+	$str2 = 'World';
+	$greeting = $str1.' '.$str2.'!';
+	$greeting2 = "$str1 $str2!!";
+
+	$str3 = 'You\'re Welcome';
+	$str4 = "You're Welcome";
+
+	$num1 = 8;
+	$num2 = 7.2;
+	$sum = $num1 + $num2;
+	
+	$bool1 = true;
+	
+	define('GREETING', 'This is a constant.', true);
+
+	echo greeting;
+?>
