93a612c6241e48070e9b8e141da5cd3ca1fc41e6
marufropy <marufropy@gmail.com>
2020-08-18 17:34:07 +0600

    variables, constants, data types and arrays updated

--- arrays.php
+++ arrays.php
@@ -1,31 +1,63 @@
 <?php
-	//Indexed
-	$name = array('maruf', 'ropy');
+	$lineBreak = "<br />";
+
+	//Indexed Array
+	$name = array('abdullah', 'al', 'maruf');
+	$name[3] = 'ropy';
+	var_dump($name);
+	echo($lineBreak);
+
+	echo($lineBreak);
+
 	$cars = ['audi', 'bmw', 'mercedes'];
-	$name[2] = 'abdullah';
-	$cars[] = 'cadillac';
-	$numbers = array(2, 7, 8);
-	
+	$cars[] = 'cadillac';	
 	echo count($cars);
+	echo($lineBreak);
+
 	print_r($cars);
+	echo($lineBreak);
+
 	var_dump($cars);
+	echo($lineBreak);
 
-	//Associative
+	echo($lineBreak);
+
+	$numbers = array(2, 7, 8);
+	var_dump($numbers);
+	echo($lineBreak);
+
+	echo($lineBreak);
+
+	//Associative Array
 	$people = array('Brad' => 35, 'Jose' => 32, 'William' => 37);
-	$ids = [22 => 'Brad', 44 => 'Jose', 63 => 'William'];
+	var_dump($people);
+	echo($lineBreak);
+
 	$people['Jill'] = 42;
+	var_dump($people);
+	echo($lineBreak);
+
+	echo($lineBreak);
+
+	$ids = [22 => 'Brad', 44 => 'Jose', 63 => 'William'];
+	var_dump($ids);
+	echo($lineBreak);
+
 	$ids[42] = 'Jill';
+	var_dump($ids);
+	echo($lineBreak);
 
-	echo $people['Jill'];
-	echo $ids[42];
-
-	//Multidimentional
+	echo($lineBreak);
+	
+	//Multidimensional Array
 	$shop = array(
 		['laptops', 50, 10],
 		['cellphones', 100, 30],
 		['headphones', 500, 120]
 	);
-	
-	echo $shop[1][2];
 	var_dump($shop);
+	echo($lineBreak);
+
+	echo $shop[1][2];
+	echo($lineBreak);
 ?>
